98bc02b03791c2f58dc1a634ecdc697a4ddeec21
CurtsApp <curtsapp@hotmail.com>
2015-09-22 14:06:15 -0700

    Confirmation Update
    Instead of using one pin it now uses 3
    Now has 100% accuracy
    Extremely quick communication times

--- src/Board.cpp
+++ src/Board.cpp
@@ -1,5 +1,5 @@
 /*
-Board.h - Library created for wired communication between arduino boards.
+Board.cpp - Library created for wired communication between arduino boards.
 by Curtis Sapp
 September 11th, 2015
 */
@@ -8,81 +8,106 @@ September 11th, 2015
 #include "Board.h"
 #include "Arduino.h"
 
+Board::Board(int dataPin_a, int inputConfirmationPin_a, int outputConfirmationPin_a, bool server) {
+
+  dataPin = dataPin_a;
+  inputConfirmationPin = inputConfirmationPin_a;
+  outputConfirmationPin = outputConfirmationPin_a;
+
+  if(server) {
+  pinMode(dataPin, OUTPUT);
+  } else {
+  pinMode(dataPin,INPUT);
+  }
+  pinMode(inputConfirmationPin, INPUT);
+  pinMode(outputConfirmationPin, OUTPUT);
+}
+
+//Must contain 8 elements
+
+void Board::sendByte(int values[]) {
+    int confirmationValue;  
+    bool firsthalf;
+
+for (int i = 0; i < 8; i++) {
+      confirmationValue = 1;
+  firsthalf = true;  
+  while(true){
+  sendBit(values[i]);
+  sendConfirmation(confirmationValue);
+  
+  if(getConfirmation() == 1 && firsthalf) {
+        confirmationValue = 0;
+    firsthalf = false;
+        }
+  if(getConfirmation() == 0 && !firsthalf) {
+    break;
+     }    
+   }
+ }
+}
 
 
+String Board::getByte() {
+  String recievedByte = "";
+    int confirmationValue;  
+    bool firsthalf;
+  for (int i = 0; i < 8; i++) {
+      confirmationValue = 0;
+  firsthalf = true;  
+  while(true){
 
+  sendConfirmation(confirmationValue);
+  
+  if(getConfirmation() == 1 && firsthalf) {
+    recievedByte = recievedByte + getBit();
+    firsthalf = false;
+    confirmationValue = 1;
+        }
+  if(getConfirmation() == 0 && !firsthalf) {
+    break;
+     }    
+   }
+  }
 
-    
+  return recievedByte;
 
-    Board::Board(int inputPin_a, int outputPin_a) {
-      inputPin = inputPin_a;
-      outputPin = outputPin_a;
-      pinMode(inputPin, INPUT);
-      pinMode(outputPin, OUTPUT);
-    }
+}
 
-
-    int Board::getInputPin() {
-      return inputPin;
-    }
-
-    int Board::getOutputPin() {
-      return outputPin;
-    }
-
-    //Must contain 8 elements
-
-    void Board::sendByte(int *values) {
-	sendBit(1);
-Serial.print(commDelay);
-        for (int i = 0; i < 8; i++) {
-	sendBit(values[i]);
-        delay(commDelay);
-      }
-	sendBit(0);
-
-    }
-	
-    bool Board::syncWait() {
-      if(getBit() == 1) {
-        delay(commDelay);
-	return true;
-	} else {
-           return false;
-      }
-    }
-
-    String Board::getByte() {
-      String recievedByte = "";
-
-      for (int i = 0; i < 8; i++) {
-        recievedByte = recievedByte + getBit();
-        delay(commDelay);
-      }
-
-      return recievedByte;
-
-    }
-
-    void Board::sendBit(int value) {
-      if (value == 0) {
-        digitalWrite(outputPin, LOW);
-      } else if (value == 1) {
-        digitalWrite(outputPin, HIGH);
-      }
-	delay(commDelay);
-
-    }
-
+void Board::sendBit(int value) {
+  if (value == 0) {
+    digitalWrite(dataPin, LOW);
+  } else if (value == 1) {
+    digitalWrite(dataPin, HIGH);
+  }
+}
 
     int Board::getBit() {
-      if (digitalRead(inputPin) == LOW) {
+      if (digitalRead(dataPin) == LOW) {
         return 0;
-      } else if (digitalRead(inputPin) == HIGH) {
+      } else if (digitalRead(dataPin) == HIGH) {
         return 1;
       }
 
     }
+void Board::sendConfirmation(int value) {
+  if (value == 0) {
+    digitalWrite(outputConfirmationPin, LOW);
+  } else if (value == 1) {
+    digitalWrite(outputConfirmationPin, HIGH);
+  }
+}
+int Board::getConfirmation() {
+  if (digitalRead(inputConfirmationPin) == LOW) {
+    return 0;
+  } else if (digitalRead(inputConfirmationPin) == HIGH) {
+    return 1;
+  }
+
+}
+
+
+
 
 
 
--- src/Board.h
+++ src/Board.h
@@ -3,26 +3,27 @@ Board.h - Library created for wired communication between arduino boards.
 by Curtis Sapp
 September 11th, 2015
 */
-#ifndef Board_h
-#define Board_h
+#ifndef Board
+#define Board
 
 #include "Arduino.h"
 
 class Board {
     
     public:
-    const int commDelay = 600;
-    int inputPin;
-    int outputPin;
-    Board(int inputPin_a, int outputPin_a);
-    int getInputPin();
-    int getOutputPin();
+    int dataPin;
+    int inputConfirmationPin; 
+    int outputConfirmationPin;
+
+
+    Board(int dataPin_a, int inputConfirmationPin_a, int outputConfirmationPin_a, bool server);
     void sendByte(int values[]);    
     bool syncWait() ;
     String getByte();
     void sendBit(int value);
     int getBit();
-
+    void sendConfirmation(int value);
+    int getConfirmation();
 };
 
 #endif
